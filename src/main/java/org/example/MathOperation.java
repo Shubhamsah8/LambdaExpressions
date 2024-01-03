4bd7ee70247ffa4677bfb408c2c2245061bc06ac
Shubhamsah8 <shubhamsah086@gmail.com>
2024-01-03 07:29:34 +0530

    MathOperation using lambda expression

--- src/main/java/org/example/MathOperation.java
+++ src/main/java/org/example/MathOperation.java
@@ -1,13 +1,30 @@
 package org.example;
 
+/**
+ * Created a Functional Interface to calculate the Arithmetic operations.
+ */
 @FunctionalInterface
 interface MathOperationInterface{
+    /**
+     * Calculate method to calculate the operations by passing two arguments.
+     * @param a taking first double value.
+     * @param b taking second double value.
+     * @return
+     */
     double calculate(double a, double b);
 }
 
+/**
+ * MathOperation class to add the Lambda expressions.
+ */
 public class MathOperation{
+    /**
+     * Some Arithmetic operation using lambda expressions.
+     */
     public static MathOperationInterface add = (a,b) -> a+b;
     public static MathOperationInterface subtract = (a,b) -> a-b;
     public static MathOperationInterface multiply = (a,b) -> a*b;
-    public static MathOperationInterface divide = (a,b) -> a/b;
+    public static MathOperationInterface divide = (a,b) -> (b!=0)? a/b: Double.NaN;
+
 }
+
--- src/test/java/org/example/MathOperationTest.java
+++ src/test/java/org/example/MathOperationTest.java
@@ -2,25 +2,49 @@ package org.example;
 import org.example.*;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+/**
+ * Created a testing class to unit test various testing methods.
+ */
 public class MathOperationTest {
+    /**
+     * Testing the add function.
+     */
 
     @Test
     public void testAdd(){
         Assert.assertEquals(11.0, MathOperation.add.calculate(5,6),0.0);
     }
 
+    /**
+     * Testing the subtract function.
+     */
     @Test
     public void testSubtract(){
         Assert.assertEquals(6.0, MathOperation.subtract.calculate(16, 10), 0.0);
     }
 
+    /**
+     * Testing the Multiply function.
+     */
     @Test
     public void testMultiply(){
         Assert.assertEquals(12, MathOperation.multiply.calculate(4, 3), 0.0);
     }
 
+    /**
+     * Testing the Divide function.
+     */
     @Test
     public void testDivide(){
         Assert.assertEquals(3, MathOperation.divide.calculate(6, 2), 0.0);
+    }
+
+    /**
+     * Testing the Divide function that will be divided by zero.
+     */
+    @Test
+    public void testDivideByZero(){
+        Assert.assertEquals(Double.NaN, MathOperation.divide.calculate(3, 0),0.0);
     }
 }
